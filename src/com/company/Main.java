271a013ab1c6ed82f1f305d8345eba452c69d50b
fvictoria <fv799@pvsd.org>
2019-02-08 11:17:43 -0500

    º.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,21 @@
 package com.company;
 import java.util.*;
-
 public class Main {
     public static ArrayList<MessageList> m1List = new ArrayList<MessageList>();
     public static void main(String[] args) {
 
         Scanner kbReader = new Scanner (System.in);
         System.out.println("Do you (1) want to send a message or (2) delete a message");
-        int log = kbReader.nextInt();
-        if(log==1)
+        int res = kbReader.nextInt();
+        if(res==1)
         {
             System.out.println("Enter recipient's name");
             String recip = kbReader.nextLine();
             System.out.println("Enter message");
             String mes = kbReader.nextLine();
-            Message m = new Message(recip,mes);
-            m1List.add(m);
+
         }
-        if(log==2)
+        if(res==2)
         {
 
         }
--- src/com/company/Message.java
+++ src/com/company/Message.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class Message {
+    String ph;
+    String mes;
+    public Message(String num,String mess )
+    {
+        ph = num;
+        mes = mess;
+
+    }
 }
